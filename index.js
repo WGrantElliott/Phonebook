9d91674f53f74878348a64197f6fa1d5e8ad901f
WGrantElliott <54452250+WGrantElliott@users.noreply.github.com>
2026-07-04 11:20:23 -0500

    adding frontend

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('body', function(req,res) {return JSON.stringify(req.body)})
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
